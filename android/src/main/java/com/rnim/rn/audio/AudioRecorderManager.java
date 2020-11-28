30598fe6af4a3c292eafa9f73f12f2ede6f9e0d8
JiHoo Hong <wlgn567@naver.com>
2020-11-28 21:44:15 +0900

    Update AudioRecorderManager.java

--- android/src/main/java/com/rnim/rn/audio/AudioRecorderManager.java
+++ android/src/main/java/com/rnim/rn/audio/AudioRecorderManager.java
@@ -251,7 +251,7 @@ class AudioRecorderManager extends ReactContextBaseJavaModule {
                   }
                 });
               }
-            }, 0, 250);
+            }, 0, 100);
         } catch (Exception e) {
      
         }
